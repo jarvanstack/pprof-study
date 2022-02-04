90b5c649af5f49963882a70032e42e29fd13cfd2
dengjiawen8955@gmail.com <dengjiawen8955@gmail.com>
2022-02-04 22:41:16 +0800

    feat(profile): 优化 profile 占用 cpu

--- pprofin/profileimpl/profile_pprof.go
+++ pprofin/profileimpl/profile_pprof.go
@@ -10,7 +10,9 @@ type PprofProfile struct {
 func (p *PprofProfile) DoPprof() {
 	fmt.Printf("%s\n", "PprofProfile")
 	//TODO: cpu 性能瓶颈
-	for i := 0; i < 10; i++ {
-		//do nothing
+	for i := 0; i < 1000_000_000; i++ {
+		f := 0.003
+		f = f*f*f/f + f
+		f = f + 1
 	}
 }
